1effa6ebef1470350544dfcfbbc12913c5bdbe65
MONKE-source <130353466+MONKE-source@users.noreply.github.com>
2024-11-02 21:43:02 +0800

    added quotes

--- kanye.js
+++ kanye.js
@@ -2,6 +2,7 @@ function apiFetchKanye() {
   fetch("https://api.kanye.rest")
     .then((response) => response.json())
     .then((data) => {
+      document.getElementById("name").innerHTML = '" ' + data.quote + ' "';
       console.log(data.quote);
     })
     .catch((error) => console.error(error));
