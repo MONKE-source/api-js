8fa7df079b43b70ae85956eeeb6029288857396a
MONKE-source <130353466+MONKE-source@users.noreply.github.com>
2024-11-02 16:29:10 +0800

    added more funcitonality

--- worlds.js
+++ worlds.js
@@ -6,9 +6,31 @@ function getCountryInfo(name, information) {
       information.forEach((element) => {
         try {
           const info = getNestedValue(data[0], element);
-          console.log(element + ": " + JSON.stringify(info, null, 2));
+          thing = JSON.stringify(info, null, 0);
+          console.log(element + ": " + thing);
+          if (element == "flags.svg") {
+            document.getElementById("flag").src = thing.slice(1, -1);
+          }
+          if (element == "name.common") {
+            document.getElementById("name").innerHTML =
+              "Country name: " + thing.slice(1, -1);
+            document.getElementById("title").innerHTML = thing.slice(1, -1);
+          }
+          if (element == "capital") {
+            document.getElementById("capital").innerHTML =
+              "Country capital: " + thing.slice(2, -2);
+          }
+          if (element == "population") {
+            document.getElementById("population").innerHTML =
+              "Country population: " + thing;
+          }
+          if (element == "area") {
+            document.getElementById("area").innerHTML =
+              "Country area: " + thing + " km²";
+          }
         } catch (error) {
           console.error("Invalid information for element: " + element);
+          alert("Invalid information");
         }
       });
     })
@@ -19,10 +41,15 @@ function getNestedValue(obj, key) {
   return key.split(".").reduce((o, k) => (o || {})[k], obj);
 }
 
-getCountryInfo("canada", [
-  "capital",
-  "population",
-  "coatOfArms",
-  "languages.eng",
-  "translations.fra.common",
-]);
+function getCountry() {
+  const name = document.getElementById("country").value;
+  event.preventDefault(); // prevent from reloading
+  getCountryInfo(name, [
+    "name.common",
+    "capital",
+    "population",
+    "area",
+    "flags.svg",
+  ]);
+}
+// splice method helps to remove elements from an array/string
